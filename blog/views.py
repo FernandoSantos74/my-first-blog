05c942617748c03bc39f5ccef442eec636e0f449
FernandoSantos74 <afernandostos@gmail.com>
2019-07-01 23:09:12 -0300

    My portfolio app, first commit

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
 # Create your views here.
+
+
+from django.shortcuts import render
+from django.http import HttpResponse
+"""
+def hello(request):
+	return render(request, 'blog/hello.html',{})
+"""
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
